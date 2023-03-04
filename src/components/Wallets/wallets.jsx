7cffedb151540484db29943935258a135b924ec7
SardiusJay <abidoyejay@gmail.com>
2023-03-04 12:29:11 +0100

    wip:Adding styling to the wallet page

--- src/components/Wallets/wallets.jsx
+++ src/components/Wallets/wallets.jsx
@@ -4,7 +4,7 @@ import { RiSettings5Fill } from 'react-icons/ri';
 const Wallets = () => {
   return (
     <div className="wallet">
-    <h1>Connect Wallet</h1>
+    <h1 className="wallet text">Connect Wallet</h1>
     <p>Open protocol for connecting Wallets to Dapps</p>
     <div classNme="wallet-crypto-image">
     <img src="/Image/brd.jpg" alt="BRD wallet" />
@@ -14,6 +14,8 @@ const Wallets = () => {
     <img src="/Image/phantom.jpeg" alt="phantom wallet" />
     <img src="/Image/cosmos.png" alt="Cosmos wallet" />
     <img src="/Image/exodus.png" alt="exodous wallet" />
+    <img src="/Image/wallet connect.png" alt="Connect Wallet" />
+    <img src="/Image/Rainbow.png" alt="Rainbow Wallet" />
     
     </div>
       
