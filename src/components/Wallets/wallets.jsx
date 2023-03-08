d7b7c45989069e235d59ba967dc7c2f4903df6b1
SardiusJay <abidoyejay@gmail.com>
2023-03-08 15:03:45 +0100

    commiting changes

---
+++ src/components/Wallets/wallets.jsx
@@ -0,0 +1,24 @@
+import './wallets.css'
+import { RiSettings5Fill } from 'react-icons/ri';
+
+const Wallets = () => {
+  return (
+    <div className="wallet">
+    <h1>Connect Wallet</h1>
+    <p>Open protocol for connecting Wallets to Dapps</p>
+    <div classNme="wallet-crypto-image">
+    <img src="/Image/brd.jpg" alt="BRD wallet" />
+    <img src="/Image/Coinbaselogo_Supplied_250x250-2.png" alt="BRD wallet" />
+    <img src="/Image/saitama.png" alt="saitama wallet" />
+    <img src="/Image/terra.png" alt="terra wallet" />
+    <img src="/Image/phantom.jpeg" alt="phantom wallet" />
+    <img src="/Image/cosmos.png" alt="Cosmos wallet" />
+    
+    </div>
+      
+    </div>
+  )
+}
+
+export default Wallets;
+
